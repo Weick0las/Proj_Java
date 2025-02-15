280b9d3a6c982d0562f78edd87ae5d872e3c0fef
Nickolas Demarchi <nickolasdemarchi@gmail.com>
2025-02-15 13:16:39 -0300

    Case 1 - analise de candidatos

---
+++ proj-java-dio/controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,19 @@
+package candidatura;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        System.out.println("Processo seletivo");
+    }
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("Ligar para o candidato");
+        }
+        else if(salarioBase == salarioPretendido){
+            System.out.println("Ligar para o candidato com contra proposta");
+        }
+        else{
+            System.out.println("Aguardando resultado dos demais candidatos");
+        }
+    }
+}
